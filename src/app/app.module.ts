c57ef13993c3a1986d0531155126921e4eb4ef69
sundar1095 <sundaramaha2@gmail.com>
2022-04-19 14:44:19 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KanbanAllModule } from '@syncfusion/ej2-angular-kanban';
-// import { TextBoxAllModule } from '@syncfusion/ej2-angular-inputs';
-import { ButtonAllModule } from '@syncfusion/ej2-angular-buttons';
-import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
-import { DialogModule } from '@syncfusion/ej2-angular-popups';
 
-import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 
 
 
@@ -22,11 +17,6 @@ import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
     BrowserModule,
     AppRoutingModule,
     KanbanAllModule,
-    // TextBoxAllModule,
-    ButtonAllModule,
-    DatePickerModule,
-    DialogModule,
-     DropDownListAllModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
